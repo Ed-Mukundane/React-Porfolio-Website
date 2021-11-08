e143491b1156753e87ece49ba58beb2b475d0733
Ed-Mukundane <aedgar990@gmail.com>
2021-11-08 15:26:45 +0300

    React-Portfolio-website

---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import "./portfolioList.scss";
+
+export default function PortfolioList({ id, title, active, setSelected }) {
+	return (
+		<li
+			className={active ? "portfolioList active" : "portfolioList"}
+			onClick={() => setSelected(id)}>
+			{title}
+		</li>
+	);
+}
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,33 @@
+import './topbar.scss';
+import {Person, Mail} from "@material-ui/icons"
+
+export default function TopBar({menuOpen, setMenuOpen}) {
+    return (
+			<div className={"topbar " + (menuOpen && "active")}>
+				<div className="wrapper">
+					<div className="left">
+						<a href="#intro" className="logo">
+							- Ampiire -
+						</a>
+						<div className="itemContainer">
+							<Person className='icon' />
+							<span>+256 779 021140</span>
+						</div>
+
+						<div className="itemContainer">
+							<Mail className='icon'/>
+							<span>aedgar990@gmail.com</span>
+						</div>
+					</div>
+
+                <div className="right">
+                    <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
+                    </div>
+				</div>
+			</div>
+		);
+}
